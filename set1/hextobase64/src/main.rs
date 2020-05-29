38494363613eea30dadd39219e7df0e7d220dccf
Devang Sampat <devangs27@gmail.com>
2020-05-28 18:08:02 -0700

    Remove intermediate variables

--- set1/hextobase64/src/main.rs
+++ set1/hextobase64/src/main.rs
@@ -1,27 +1,13 @@
-/*
-Convert hex to base64
-The string:
-
-49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-Should produce:
-
-SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-*/
-
 extern crate data_encoding;
 
 use data_encoding::{HEXLOWER, DecodeError};
 use data_encoding::BASE64;
 
+/*
+Convert lowercase hex to base64
+ */
 fn hextobase64(input: &[u8;96]) -> Result<String, DecodeError>{
-
-  
-  let binary = HEXLOWER.decode(input)?;
-  let result = BASE64.encode(&binary);
-
-  println!("{}",result);
-
-  Ok(result)
+  Ok(BASE64.encode(&HEXLOWER.decode(input)?))
 }
 
 fn main() {
@@ -29,14 +15,11 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-  // Note this useful idiom: importing names from outer (for mod tests) scope.
   use super::*;
 
   #[test]
   fn test_simple() {
     let input = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-
-    let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    assert_eq!(hextobase64(input).unwrap(), expected);
+    assert_eq!(hextobase64(input).unwrap(), "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";);
   }
 }
