a3510e2b05678c01a37036c1d18b1130f4e109f2
Devang Sampat <devangs27@gmail.com>
2020-05-28 18:11:15 -0700

    Modify hextobase64 input to accept string instead of binary representation

--- set1/hextobase64/src/main.rs
+++ set1/hextobase64/src/main.rs
@@ -6,8 +6,8 @@ use data_encoding::BASE64;
 /*
 Convert lowercase hex to base64
  */
-fn hextobase64(input: &[u8;96]) -> Result<String, DecodeError>{
-  Ok(BASE64.encode(&HEXLOWER.decode(input)?))
+fn hextobase64(input: &str) -> Result<String, DecodeError>{
+  Ok(BASE64.encode(&HEXLOWER.decode(input.as_bytes())?))
 }
 
 fn main() {
@@ -19,7 +19,7 @@ mod tests {
 
   #[test]
   fn test_simple() {
-    let input = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    assert_eq!(hextobase64(input).unwrap(), "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";);
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    assert_eq!(hextobase64(input).unwrap(), "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
   }
 }
