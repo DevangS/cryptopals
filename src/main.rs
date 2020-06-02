7022505e271a68aa06d3f42c5c6d7ce429446084
Devang Sampat <devangs27@gmail.com>
2020-06-01 23:43:50 -0700

    Set 1 challenge 3: Simple XOR cipher cracker

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate data_encoding;
 
 use data_encoding::{HEXLOWER, DecodeError};
 use data_encoding::BASE64;
+use std::collections::HashMap;
+use std::borrow::Borrow;
 
 /*
 Convert lowercase hex to base64
@@ -22,6 +24,65 @@ fn xor(left: &str, right: &str) ->Result<String, DecodeError>{
       zip(right_bytes.iter()).
       for_each(|(lb, rb)| *lb ^= *rb);
   Ok(HEXLOWER.encode(&left_bytes))
+}
+
+/*
+Decrypt a string that has been XOR'd with a single character
+ */
+fn single_byte_xor_decrypt(input: &str) -> Result<String, DecodeError>{
+  //create frequency map
+  let _magic = "ETAOIN SHRDL";
+  let mut counts: HashMap<u8, u32> = HashMap::with_capacity(_magic.len());
+  for m in _magic.chars() {
+    counts.insert(m as u8, 0);
+  }
+
+  //allocate space to track most likely valid plaintext
+  let mut best_score = 0;
+  let mut result = Vec::new();
+  //convert hex input string to bytes
+  let str_bytes = HEXLOWER.decode(input.as_bytes()).unwrap();
+
+  //choose a 1 byte char from 'A' to 'Z'
+  for c in 65..91{
+    //allocate memory for this plaintext
+    let mut plaintext = Vec::with_capacity(str_bytes.len());
+
+    //reset frequency map to 0
+    for (_, val) in counts.iter_mut() {
+      *val = 0;
+    }
+
+    for e in str_bytes.iter(){
+      let decrypted = *e ^ c;
+      let mut key = decrypted;
+
+      //try to capitalize if it's a possible lowercase letter
+      if decrypted > 96 {
+        key = decrypted - 32;
+      }
+
+      //Update frequency map with count of relevant chars
+      if let Some(x) = counts.get_mut(&key){
+        match x.checked_add(1) {
+          Some(v) => {
+            *x = v;
+          }
+          None => {}
+        }
+      }
+      plaintext.push(decrypted);
+    }
+
+    let score = counts.values().sum();
+
+    if score > best_score{
+      best_score = score;
+      result = plaintext.clone(); //should result be &mut Vec<u8> instead? TODO: learn more about ownership
+    }
+  }
+
+  Ok(String::from_utf8(result).expect("invalid"))
 }
 
 fn main() {
@@ -48,5 +109,10 @@ mod tests {
   fn test_xor_mismatched_str_length(){
     assert!(xor("1c0111001f010100061a024b53535009181", "686974207468652062756c6c277320657965").is_err(),
     "String length is mismatched");
+  }
+
+  #[test]
+  fn test_single_byte_xor_decrypt(){
+    assert_eq!(single_byte_xor_decrypt("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap(), "Cooking MC's like a pound of bacon");
   }
 }
